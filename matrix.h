c19caa0cd1420eda09b785ad80a5fd4edb08bb58
Zartarius <zaryabayaz2@gmail.com>
2025-01-07 12:19:42 +1100

    Update matrix.h
    Add more prototypes.

--- matrix.h
+++ matrix.h
@@ -1,3 +1,6 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
 #include <stdlib.h>
 
 typedef struct {
@@ -7,7 +10,13 @@ typedef struct {
 } matrix_t;
 
 matrix_t zeroes(const size_t m, const size_t n);
+matrix_t random_matrix(const size_t m, const size_t n);
+static inline float float_abs(float num);
+void normalise(matrix_t matrix);
+void matrix_add_vector(matrix_t matrix, matrix_t vector);
 void free_matrix(matrix_t matrix);
 matrix_t transpose(matrix_t matrix);
 matrix_t multiply(matrix_t a, matrix_t b);
 void print_matrix(matrix_t matrix);
+
+#endif
