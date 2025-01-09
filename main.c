ab7c35fc3e3216f612c8e2da314b87f87d0fc76e
Zartarius <zaryabayaz2@gmail.com>
2025-01-09 18:38:29 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include "matrix.h"
+#include "parse_csv.h"
+// #include <stdio.h>
 
 int main() {
-    matrix_t a = random_matrix(1000, 1500);
-    matrix_t b = transpose(a);
+    // matrix_t* data = read_csv("test.csv", 0, true);
+    // print_matrix(data[0]);
+    // print_matrix(data[1]);
+    matrix_t a = zeroes(10, 10);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            a.values[i][j] = (float) i == j;
+        }
+    }
     print_matrix(a);
+
+    matrix_t b = random_matrix(10, 10);
     print_matrix(b);
     print_matrix(multiply(a, b));
 }
