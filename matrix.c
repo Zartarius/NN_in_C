5f1752fb63c38fcf26cec10f9a1344499d6eea6d
Zartarius <zaryabayaz2@gmail.com>
2025-01-10 16:07:00 +1100

    Convert to 1D array

--- matrix.c
+++ matrix.c
@@ -18,13 +18,9 @@ matrix_t zeroes(const size_t m, const size_t n) {
     matrix_t matrix;
     matrix.m = m;
     matrix.n = n;
-    matrix.values = (float**)malloc(m * sizeof(float*));
+    matrix.values = (float*) calloc(m * n, sizeof(float));
     assert(matrix.values != NULL);
 
-    for (size_t i = 0; i < m; i++) {
-        matrix.values[i] = (float*)calloc(n, sizeof(float));
-        assert(matrix.values[i] != NULL);
-    }
     return matrix;
 }
 
@@ -35,31 +31,29 @@ matrix_t random_matrix(const size_t m, const size_t n) {
     matrix_t matrix;
     matrix.m = m;
     matrix.n = n;
-    matrix.values = (float**)malloc(m * sizeof(float*));
+    matrix.values = (float*) malloc(m * n * sizeof(float));
     assert(matrix.values != NULL);
 
     for (size_t i = 0; i < m; i++) {
-        matrix.values[i] = (float*)malloc(n * sizeof(float));
-        assert(matrix.values[i] != NULL);
-    }
-    for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < n; j++) {
-            matrix.values[i][j] = (float)rand() / (float)RAND_MAX;
+            matrix.values[i * n + j] = (float) rand() / (float) RAND_MAX;
         }
     }
     return matrix;
 }
 
 // Private helper function
-static inline float float_abs(float num) { return (num < 0) ? -num : num; }
+static inline float float_abs(float num) { 
+    return (num < 0) ? -num : num; 
+}
 
 // Normalises a matrix to have values between -1 and 1
 void normalise(matrix_t matrix) {
-    float max = float_abs(matrix.values[0][0]);
+    float max = float_abs(matrix.values[0]);
 
     for (size_t i = 0; i < matrix.m; i++) {
         for (size_t j = 0; j < matrix.n; j++) {
-            float num = float_abs(matrix.values[i][j]);
+            float num = float_abs(matrix.values[i * matrix.n + j]);
             max = (num > max) ? num : max;
         }
     }
@@ -68,7 +62,7 @@ void normalise(matrix_t matrix) {
     }
     for (size_t i = 0; i < matrix.m; i++) {
         for (size_t j = 0; j < matrix.n; j++) {
-            matrix.values[i][j] /= max;
+            matrix.values[i * matrix.n + j] /= max;
         }
     }
 }
@@ -79,26 +73,18 @@ void matrix_add_vector(matrix_t matrix, matrix_t vector) {
 
     for (size_t i = 0; i < matrix.m; i++) {
         for (size_t j = 0; j < matrix.n; j++) {
-            matrix.values[i][j] += vector.values[0][j];
+            matrix.values[i * matrix.n + j] += vector.values[j];
         }
     }
-}
-
-// Frees a matrix
-void free_matrix(matrix_t matrix) {
-    for (size_t i = 0; i < matrix.m; i++) {
-        free(matrix.values[i]);
-    }
-    free(matrix.values);
 }
 
 // Returns the transpose of a matrix
 matrix_t transpose(matrix_t original) {
     matrix_t transposed = zeroes(original.n, original.m);
 
-    for (size_t i = 0; i < transposed.m; i++) {
-        for (size_t j = 0; j < transposed.n; j++) {
-            transposed.values[i][j] = original.values[j][i];
+    for (size_t i = 0; i < original.m; i++) {
+        for (size_t j = 0; j < original.n; j++) {
+            transposed.values[j * transposed.n + i] = original.values[i * original.n + j];
         }
     }
     return transposed;
@@ -117,12 +103,12 @@ static void* multiply_row(void* arg) {
         size_t k = 0;
         if (a->n > 8) {
             for (; k <= a->n - 8; k += 8) {
-                __m256 a_vec = _mm256_load_ps(&a->values[row][k]);
+                __m256 a_vec = _mm256_load_ps(&a->values[row * a->n + k]);
                 __m256 b_vec =
-                    _mm256_set_ps(b->values[k + 7][j], b->values[k + 6][j],
-                                  b->values[k + 5][j], b->values[k + 4][j],
-                                  b->values[k + 3][j], b->values[k + 2][j],
-                                  b->values[k + 1][j], b->values[k][j]);
+                    _mm256_set_ps(b->values[(k + 7) * b->n + j], b->values[(k + 6) * b->n + j],
+                                  b->values[(k + 5) * b->n + j], b->values[(k + 4) * b->n + j],
+                                  b->values[(k + 3) * b->n + j], b->values[(k + 2) * b->n + j],
+                                  b->values[(k + 1) * b->n + j], b->values[k * b->n + j]);
                 mul = _mm256_add_ps(_mm256_mul_ps(a_vec, b_vec), mul);
             }
             float array_mul[8];
@@ -132,9 +118,9 @@ static void* multiply_row(void* arg) {
             // for remaining
         }
         for (; k < a->n; k++) {
-            sum += a->values[row][k] * b->values[k][j];
+            sum += a->values[row * a->n + k] * b->values[k * b->n + j];
         }
-        c->values[row][j] = sum;
+        c->values[row * c->n + j] = sum;
     }
     return NULL;
 }
@@ -152,7 +138,7 @@ matrix_t multiply(matrix_t a, matrix_t b) {
         thread_data[i].b = &b;
         thread_data[i].c = &c;
         thread_data[i].row = i;
-        pthread_create(&threads[i], NULL, multiply_row, (void*)&thread_data[i]);
+        pthread_create(&threads[i], NULL, multiply_row, (void*) &thread_data[i]);
     }
     for (size_t i = 0; i < c.m; i++) {
         pthread_join(threads[i], NULL);
@@ -169,9 +155,9 @@ void print_matrix(matrix_t matrix) {
         printf("| ");
         for (size_t j = 0; j < matrix.n; j++) {
             if (j < matrix.n - 1) {
-                printf("%.2f   ", matrix.values[i][j]);
+                printf("%.2f   ", matrix.values[i * matrix.n + j]);
             } else {
-                printf("%.2f ", matrix.values[i][j]);
+                printf("%.2f ", matrix.values[i * matrix.n + j]);
             }
         }
         printf("|\n");
