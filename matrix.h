accb7b773ab3d1ae1f53aa95ebac22f122136e0d
Zartarius <zaryabayaz2@gmail.com>
2025-01-10 17:53:39 +1100

    Remove static prototypes

--- matrix.h
+++ matrix.h
@@ -11,13 +11,10 @@ typedef struct {
 
 matrix_t zeroes(const size_t m, const size_t n);
 matrix_t random_matrix(const size_t m, const size_t n);
-static inline float float_abs(float num);
 void normalise(matrix_t matrix);
 void matrix_add_vector(matrix_t matrix, matrix_t vector);
 matrix_t transpose(matrix_t matrix);
-static void *compute_tile(void *arg);
 matrix_t matrix_tile_multiply(matrix_t a, matrix_t b);
-// static void* multiply_row(void* arg);
 // matrix_t multiply(matrix_t a, matrix_t b);
 void print_matrix(matrix_t matrix);
 
