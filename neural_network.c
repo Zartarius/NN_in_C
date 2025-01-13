f1945fb5fd6f4f50e70487c204546b3fdb8b974c
Zartarius <zaryabayaz2@gmail.com>
2025-01-13 11:30:09 +1100

    Update neural_network.c

--- neural_network.c
+++ neural_network.c
@@ -1,10 +1,10 @@
 #include "neural_network.h"
 #include <assert.h>
-
-#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+#define TILE_SIZE 8
 
 // Private struct for representing a single NN layer
 typedef struct {
@@ -12,39 +12,49 @@ typedef struct {
     matrix_t biases;
 } layer_t;
 
-static layer_t* layers;
-static size_t num_layers = 0;
-
-#define TILE_SIZE 8
-
 size_t tile_size = TILE_SIZE;
 
-void init_layers(size_t* layer_info, const size_t size_layer_info) {
+static layer_t* layers;
+static size_t num_layers = 0;
+static float (*activation_func)(float input);
+
+void create_network(size_t* layer_info, const size_t size_layer_info) {
     assert(size_layer_info >= 2); // Ensure there are at least input and output layers
 
     num_layers = size_layer_info;
     layers = (layer_t*) malloc(num_layers * sizeof(layer_t)); // Allocate memory for layers
 
-    for (size_t i = 0; i < num_layers; i++) {
+    for (size_t i = 1; i < num_layers; i++) {
         // Initialize biases to zero
-        layers[i].biases = zeroes(1, layer_info[i]); // Create a 1 x neurons_in_layer matrix for biases
+        // layers[i].biases = zeroes(1, layer_info[i]); // Create a 1 x neurons_in_layer matrix for biases
         
 
-        if (i == 0) { // Skip the input layer
-            continue;
-        }
+        // if (i == 0) { // Skip the input layer
+        //     continue;
+        // }
 
-        // size_t prev_layer_neurons = layer_info[i - 1]; 
-        layers[i].weights = random_matrix(layer_info[i - 1], layer_info[i]); // Create weights matrix
+        size_t prev_layer_neurons = layer_info[i - 1]; 
+        // layers[i].weights = random_matrix(layer_info[i - 1], layer_info[i]); // Create weights matrix
+        layers[i].weights = zeroes(layer_info[i - 1], layer_info[i]);
 
-        /*
         double stddev = sqrt(2.0 / prev_layer_neurons); // Standard deviation for He initialization
         for (size_t j = 0; j < prev_layer_neurons; j++) {
             for (size_t k = 0; k < layer_info[i]; k++) {
-                layers[i].weights->data[j][k] = ((double)rand() / RAND_MAX) * 2 * stddev - stddev; // Random weights
+                layers[i].weights.values[j * prev_layer_neurons + k] = ((float) rand() / RAND_MAX) * 2 * stddev - stddev; 
             }
         }
-        */
+    }
+}
+
+result_t predict(matrix_t X) {
+    matrix_t input = X;
+
+    for (size_t i = 1; i < num_layers; i++) {
+        matrix_t output = matrix_tile_multiply(input, layers[i].weights);
+        matrix_t bias = layers[i].biases;
+        for (size_t j = 0; j < output.n; j++) {
+            output.values[j] = activation_func(output.values[i] + bias.values[0]);
+        }
     }
 }
 
