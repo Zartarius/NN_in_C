dc35780e56e55e000101e928fba63d76292eb284
Zartarius <zaryabayaz2@gmail.com>
2025-01-13 11:31:08 +1100

    Update neural_network.h

--- neural_network.h
+++ neural_network.h
@@ -16,7 +16,6 @@ typedef struct {
     size_t prediction;
 } result_t;
 
-void init_layers(size_t* layer_info, const size_t size_layer_info);
-// result_t predict()
-
+void create_network(size_t* layer_info, const size_t size_layer_info);
+result_t predict(matrix_t X);
 #endif
