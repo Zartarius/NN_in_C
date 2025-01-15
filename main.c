73d9f7cbaa8a36885829c1e87e0a80f3184fb80f
Oliver Po <oliver.po2882@gmail.com>
2025-01-15 16:05:54 +1000

    Fix merge conflict 2

--- main.c
+++ main.c
@@ -13,13 +13,8 @@ int main() {
     size_t num_layers = sizeof(layer_info) / sizeof(size_t);
 
     create_network(layer_info, num_layers);
-<<<<<<< HEAD
-    matrix_t inputs = random_matrix(1, 784);
-    result_t *predictions = predict(inputs);
-=======
     matrix_t inputs = random_matrix(num_samples, num_parameters);
     result_t* predictions = predict(inputs);
->>>>>>> a8d6eca9a26e324306dc5745a960c46305a4c24e
 
     for (size_t i = 0; i < num_samples; i++) {
         printf("%zu\n", predictions[i].prediction);
