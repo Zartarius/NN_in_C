eac13516272e38908ef9582430e1ab0e54337d38
Zartarius <zaryabayaz2@gmail.com>
2025-01-16 12:29:32 +1100

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include "train/activation.h"
 
 int main() {
-    // determine_cache(); // COMMENT THIS OUT IF YOU ARE A MAC USER
+    determine_cache(); // This will not work on non-windows systems
     size_t num_parameters = 784;
-    size_t num_samples = 20;
+    size_t num_samples = 500;
     size_t num_classes = 10;
     
     size_t layer_info[] = {num_parameters, 256, 128, num_classes};
