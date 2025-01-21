0b3359d79dc7f1efbe5702543949f5e3ee5e67bc
Zartarius <zaryabayaz2@gmail.com>
2025-01-21 11:43:02 +1100

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -12,9 +12,12 @@ typedef struct {
 matrix_t zeroes(const size_t m, const size_t n);
 matrix_t random_matrix(const size_t m, const size_t n);
 void normalise(matrix_t matrix);
-void matrix_add_vector(matrix_t matrix, matrix_t vector);
+matrix_t matrix_add_vector(matrix_t matrix, matrix_t vector);
 matrix_t transpose(matrix_t matrix);
 matrix_t matrix_tile_multiply(matrix_t a, matrix_t b);
+float add(float x, float y);
+float multiply(float x, float y);
+matrix_t matrix_apply(matrix_t* a, matrix_t* b, const float scalar, float (*function)(float, float));
 void print_matrix(matrix_t matrix);
 void determine_cache(void);
 #endif
